667f55bf95de24412c1ced3ef63581dc9d179a49
FireOOO <42704111+FireOOO@users.noreply.github.com>
2022-05-23 22:40:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="https://jsproxy.abcfire.workers.dev";
+        url.hostname="https://go.990990.xyz";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
